a5798c95ac3e771e222357977e8789fe707ff7be
Joseph <josephcaillet@gmail.com>
2016-10-27 19:32:32 +0200

    add gui to documentation export plugin

--- src/plugin/exporter/DefaultDiderotDocumentationExporter.java
+++ src/plugin/exporter/DefaultDiderotDocumentationExporter.java
@@ -1,7 +1,9 @@
 package plugin.exporter;
 
 import org.w3c.dom.Document;
+import plugin.PluginsSettings;
 
+import javax.swing.*;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
@@ -11,8 +13,7 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import java.awt.*;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
 
 /**
@@ -25,6 +26,75 @@ public class DefaultDiderotDocumentationExporter extends DefaultDiderotProjectEx
 	static
 	{
 		availableOperations.put("Generate documentation", "generateHtmlDocumentation");
+	}
+
+	//method strongly inspired by kayz1's solution here: http://stackoverflow.com/questions/6214703/copy-entire-directory-contents-to-another-directory
+	static public boolean copyDirectory(File source, File destination)
+	{
+		if (source.isDirectory())
+		{
+			if (!destination.exists())
+			{
+				destination.mkdirs();
+			}
+
+			String files[] = source.list();
+
+			for (String file : files)
+			{
+				File srcFile = new File(source, file);
+				File destFile = new File(destination, file);
+
+				if(!copyDirectory(srcFile, destFile))
+				{
+					return false;
+				}
+			}
+		}
+		else
+		{
+			InputStream in = null;
+			OutputStream out = null;
+
+			try
+			{
+				in = new FileInputStream(source);
+				out = new FileOutputStream(destination);
+
+				byte[] buffer = new byte[1024];
+
+				int length;
+				while ((length = in.read(buffer)) > 0)
+				{
+					out.write(buffer, 0, length);
+				}
+			}
+			catch (Exception e)
+			{
+				try
+				{
+					in.close();
+				}
+				catch (IOException e1)
+				{
+					e1.printStackTrace();
+					return false;
+				}
+
+				try
+				{
+					out.close();
+				}
+				catch (IOException e1)
+				{
+					e1.printStackTrace();
+					return false;
+				}
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 	@Override
@@ -65,6 +135,32 @@ public class DefaultDiderotDocumentationExporter extends DefaultDiderotProjectEx
 
 	public void generateHtmlDocumentation()
 	{
+		String fileName = PluginsSettings.getValue(getPluginName(), "documentationFolder");
+		if(fileName == null)
+		{
+			fileName = PluginsSettings.getValue("Diderot default project exporter", "projectFileName");
+			if(fileName == null)
+			{
+				fileName = ".";
+			}
+		}
+
+
+		JFileChooser fileChooser = new JFileChooser(fileName);
+		fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		if(JFileChooser.APPROVE_OPTION != fileChooser.showOpenDialog(parent))
+		{
+			return;
+		}
+
+		String docPath = fileChooser.getSelectedFile().getAbsolutePath();
+		if(!docPath.endsWith("/"))
+		{
+			docPath = docPath + "/";
+		}
+
+		PluginsSettings.setValue(getPluginName(), "documentationFolder", docPath);
+
 		try
 		{
 			Document xmlSaveDocument = createDocument();
@@ -75,8 +171,11 @@ public class DefaultDiderotDocumentationExporter extends DefaultDiderotProjectEx
 			xmlTransformer.setOutputProperty(OutputKeys.INDENT, "yes");
 			xmlTransformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
 
-			File f = new File("doc/index.html");
+			copyDirectory(new File("doc/"), new File(docPath));
+
+			File f = new File(docPath + "index.html");
 			xmlTransformer.transform(new DOMSource(xmlSaveDocument), new StreamResult(f));
+
 
 			Desktop.getDesktop().browse(f.toURI());
 		}
