9bfb23961237e376236d09b8df8cbb84b6aa2be8
Joseph <josephcaillet@gmail.com>
2017-01-18 14:28:11 +0100

    javadoc on DiderotProjectExporter

--- src/plugin/exporter/DiderotProjectExporter.java
+++ src/plugin/exporter/DiderotProjectExporter.java
@@ -6,15 +6,24 @@ import plugin.OperationNameIcon;
 import java.util.HashMap;
 
 /**
- * Created by joseph on 04/10/16.
+ * The interface Diderot project exporter, use to export project.
  */
 public interface DiderotProjectExporter extends DiderotPlugin
 {
+	/**
+	 * Encode new line string, replacing "&#xA;" by "\n".
+	 * @param str the string to encode
+	 * @return the decoded string
+	 */
 	static public String encodeNewLine(String str)
 	{
 		return str.replace("\n", "&#xA;");
 	}
 
+	/**
+	 * Gets available exporting operations.
+	 * @return the available exporting operations
+	 */
 	//Todo: add icon per action support
 	public HashMap<String, OperationNameIcon> getAvailableExportingOperations();
 }
